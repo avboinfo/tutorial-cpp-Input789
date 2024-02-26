3dc64250ac100b019917ef420442c3154ae20c43
Input789 (matteo.galdini) <134801144+Input789@users.noreply.github.com>
2024-02-26 08:11:29 +0000

    Struct

--- E_Strutture/prova_struct.cpp
+++ E_Strutture/prova_struct.cpp
@@ -1,8 +1,6 @@
-/*
-** prova_struct.cpp
-** Author: IAV 3CIN
-** 28/02/2021
-*/
+/* Prove con struct
+GALDINI MATTEO 3CIN*/
+
 
 #include <iostream>
 
@@ -13,19 +11,38 @@ using namespace std;
 ** attraverso il nome unico della struttura.
 */
 
-struct numeri
-{
-    int val_uno;
-    float val_due;
+struct persona{
+  string nome1;
+  int mese_di_nascita;
+  int anno_di_nascita;
 };
 
+typedef struct ciao{
+  int c1;
+  int c2;
+}CIAO;
+
+typedef int ciaone;
+
+typedef struct {int c1; int c2;} C;
 
 int main()
 {
-  struct numeri n;
-  n.val_uno = 100;
-  cout <<  "1) n.val_uno = " << n.val_uno << " - " << "n.val_due = " << n.val_due << endl;
-  n.val_due = 33.33;
-  cout <<  "2) n.val_uno = " << n.val_uno << " - " << "n.val_due = " << n.val_due << endl;
+  C caratteristica; // Usa una variabile
+  caratteristica.c1; // Usa le caratteristiche di C
+  caratteristica.c2;
+  ciaone ciaooo = 120; // Ora gli interi si chiamano ciaone
+  CIAO ciaoo; // Versione alternativa dello struct semplice
+  struct persona nm, nm2; // Struct semplice
+  cin>>nm.nome1;
+  cin>>nm.mese_di_nascita;
+  cin>>nm.anno_di_nascita;
+  cin>>nm2.nome1;
+  cin>>nm2.mese_di_nascita;
+  cin>>nm2.anno_di_nascita;
+  nm.anno_di_nascita = 2024 - nm.anno_di_nascita;
+  nm2.anno_di_nascita = 2024 - nm2.anno_di_nascita;
+  cout<<"Il primo nome è: "<<nm.nome1<<endl<<"Il secondo nome è: "<<nm2.nome1<<endl;
+  cout<<nm.nome1<<" quest'anno compie "<<nm.anno_di_nascita<<" anni"<<endl<<nm2.nome1<<" quest'anno compie "<<nm2.anno_di_nascita<<" anni"<<endl;
   return 0;
 }
