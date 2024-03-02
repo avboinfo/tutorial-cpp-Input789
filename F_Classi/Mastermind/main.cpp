46cfcb4e61077c7dd926daeabccdc25466068c23
Input789 (matteo.galdini) <134801144+Input789@users.noreply.github.com>
2024-03-02 11:39:15 +0000

    Mastermind

---
+++ F_Classi/Mastermind/main.cpp
@@ -0,0 +1,25 @@
+/* Mastermind with CPP
+Galdini Matteo 02/03/2024*/
+
+
+ #include <iostream>
+
+ #include "mastermind.cpp" 
+
+
+ using namespace std;
+
+
+ int main(){
+
+    mastermind first = mastermind();
+    //mastermind start = mastermind();
+
+    //start.startGame();
+
+    first.newShot();
+
+
+
+    return 0;
+ }
